bf9ee923bb92bc728f558ea590f3535bf1688ef9
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-08 19:33:01 -0700

    Added shout out to the background author

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,12 @@ const Footer = () => {
     return (
         <footer>
             <span>Mykyta Medvediev 2022</span>
+            <span>
+                <a href="http://www.pexels.com/photo/green-vegetables-977893/">
+                    Background
+                </a>
+                by Dominika Roseclay
+            </span>
         </footer>
     );
 };
