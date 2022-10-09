14b9e340ab98987eb966dde67b6ccdba6b83b092
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-08 19:33:15 -0700

    Some background filler

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div id="home">
-            Home to a beautiful, modern flower shop. <br />
+            <h3>Home to a beautiful, modern flower shop.</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores obcaecati tenetur necessitatibus quae. Esse
+                asperiores modi laudantium aut dolores! Eaque, aliquam quae
+                eveniet sit laborum nihil quibusdam quos numquam laudantium.
+            </p>
             <Link to="/shop">
                 <span>Shop</span>
             </Link>
